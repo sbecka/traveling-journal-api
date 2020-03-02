474e7b9d58878a2b583f4df498755bea990ccec7
Sabrina Becker <sbecka8@gmail.com>
2020-03-02 10:31:52 -0500

    Set cors

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
