66c1922bee4b733dc44ff1b2b538e1de0e45757b
Sabrina Becker <sbecka8@gmail.com>
2020-03-06 23:19:38 -0500

    Fix client origin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: "https://traveling-journal-app.now.sh/",
+    CLIENT_ORIGIN: "https://traveling-journal-app.now.sh",
     DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/traveling-journals",
     JWT_SECRET: process.env.JWT_SECRET || 'change-secret'
 };
