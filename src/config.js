0816fb13cef6f72497b9416531a672de102fb6fb
Sabrina Becker <sbecka8@gmail.com>
2020-03-11 23:57:24 -0400

    Configure for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: "https://traveling-journal-app.now.sh",
+    CLIENT_ORIGIN: "https://traveling-journal.now.sh",
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/traveling-journals",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/traveling-journals-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-secret',
